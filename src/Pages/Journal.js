4e977864b9e3f095f8d32838441d74ea7daa08ba
AdityaBhadauria <adityab5262@gmail.com>
2024-04-07 12:38:07 +0530

    Made some normal changes

--- src/Pages/Journal.js
+++ src/Pages/Journal.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import './Journal.css';
+import Gojo from '../images/GojoChibi.jpg'
 
 const Journal = () => {
   return (
-    <div>
-      <h1>Welcome To Journal!</h1>
-      <p>This is the Journal page of your React application.</p>
+    <div class="container">
+    <div class="side-column">
+      <div>
+      <img src={Gojo} alt="Gojo" style={{ height: '250px', marginRight: '10px' }} />
+      
+      </div>
+      <h2>Aditya Bhadauria</h2>
+      <p>Content for the left side column.</p>
     </div>
+    <div class="main-content">
+      <h2>Main Content Area</h2>
+      <p>This is the main content area of the webpage. It will take up the remaining space after the side columns.</p>
+    </div>
+    
+  </div>
+
   );
 };
 
